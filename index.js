dd4ac1999266c5bc555fe2b28878126282ea3a10
Dustin McQuay <dmcquay@gmail.com>
2017-07-07 23:31:07 -0600

    initial version of grace's adventure game

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const data = require('./adventure.json')
+
+const inquirer = require('inquirer')
+
+function handleStoryCard(card) {
+    process.stdout.write("\u001b[2J\u001b[0;0H")
+    for (let body of card.bodies) {
+        console.log(body)
+    }
+
+
+    if (card.prompt && card.answers) {
+        console.log('')
+        inquirer.prompt([{
+                type: 'list',
+                name: 'main',
+                message: card.prompt,
+                choices: card.answers.map(a => ({name: a.value, value: a.next}))
+            }])
+            .then(answers => {
+                handleStoryCard(data.storyCards[answers.main])
+            })
+    } else {
+        console.log("\nTHE END!\n\n")
+    }
+}
+
+handleStoryCard(data.storyCards.start)
